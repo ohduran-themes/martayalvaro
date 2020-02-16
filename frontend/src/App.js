04eeb9523210209e7da7dc429d6fe97e8a18f775
ohduran <alvaro.duranb@hotmail.com>
2020-02-16 22:09:17 +0100

    sub navbar, fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
+import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -9,6 +10,8 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import { LinkContainer } from 'react-router-bootstrap';
+
+import image2 from './images/image2.png';
 
 import './App.css';
 import './fonts.css';
@@ -33,7 +36,7 @@ const App = () => (
           </Row>
           <Row className="">
             <Col>
-              <h1 className="pt-1 text-center couple-vertical-and">and</h1>
+              <h1 className="pt-1 text-center font-family-elegant">&</h1>
             </Col>
           </Row>
           <Row className="">
@@ -45,16 +48,18 @@ const App = () => (
       </Row>
 
       {/* NAVBAR */}
-      <Navbar bg="light" variant="light">
-      <LinkContainer to="/"><Navbar.Brand>Home</Navbar.Brand></LinkContainer>
+      <Container>
+        <Navbar bg="light" variant="light">
+        <LinkContainer to="/"><Navbar.Brand>Home</Navbar.Brand></LinkContainer>
 
-        <Nav className="mr-auto">
+        <Nav>
           <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
           <LinkContainer to="/users"><Nav.Link>Users</Nav.Link></LinkContainer>
         </Nav>
-      </Navbar>
+        </Navbar>
+      </Container>
 
-      <Row>
+      {/* <Row>
         <Col>
           <h2>
             Current Page is{' '}
@@ -71,7 +76,20 @@ const App = () => (
             </Switch>
           </h2>
         </Col>
+      </Row> */}
 
+      <Row className="mt-3 sub-profile-container">
+        <Col className="text-center">
+        <h5>Please join us for our wedding celebration on</h5>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mt-3 text-center font-family-elegant">
+          <h1>3rd October, 2020</h1>
+        </Col>
+      </Row>
+      <Row className="mt-2 justify-content-center">
+        <Image src={image2} fluid />
       </Row>
     </Container>
   </MemoryRouter>
