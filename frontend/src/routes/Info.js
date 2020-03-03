2ebfc3babf81dc416a2fca03889c1b6040ab2dba
ohduran <alvaro.duranb@hotmail.com>
2020-03-03 19:25:26 +0100

    info WIP

--- frontend/src/routes/Info.js
+++ frontend/src/routes/Info.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import image4 from '../images/image4.jpg';
+import staagnes from '../images/staagnes.jpg';
 import Image from 'react-bootstrap/Image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapPin } from '@fortawesome/free-solid-svg-icons'
 
 export class Info extends React.Component {
   render() {
 
       return (
         <React.Fragment>
+        <Row>
+          <Col><h2 className="text-center my-3">{this.props.t('Ceremonia - 12.30')}</h2></Col>
+        </Row>
         <Row className="justify-content-center">
           <Col lg={4}>
             <Row>
               <Col>
-                <Image src={image4} className="framed-image" fluid/>
+                <Image src={staagnes} className="framed-image" fluid/>
               </Col>
             </Row>
           </Col>
-          <Col lg={6}>
+          <Col lg={8}>
             <Row>
-            <Col>
-              <p>{this.props.t('La ceremonia comenzará en la iglesia de ')} <a href="https://www.tripadvisor.es/Attraction_Review-g6560687-d8779834-Reviews-Iglesia_Santa_Agnes_de_Malanyanes-Santa_Agnes_de_Malanyanes_La_Roca_del_Valles_C.html">Santa Agnes de Malanyanes</a> {this.props.t("a eso de las 12.30. Para los que vienen de lejos, os recomendamos que hagáis noche en Barcelona, ya que fletaremos un autobús que os lleve a la iglesia.")}</p>
-              <p>{this.props.t("Tras el 'Sí, quiero', iremos a celebrarlo al ")} <a href="https://www.masdesantllei.com/">Mas de Sant Lleí</a>.</p>
-            </Col>
+              <Col>
+                <p className="text-justify">{this.props.t("Nos vemos a las 12.30")}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p><FontAwesomeIcon icon={faMapPin}/> Plaza Del Obispado, Santa Agnès de Malanyanes, La Roca del Vallès, España</p>
+              </Col>
             </Row>
           </Col>
         </Row>
