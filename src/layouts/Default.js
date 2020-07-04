58a2ec0a58bf142495b641f28e7c8bfd6a33f13d
ohduran <alvaro.duranb@hotmail.com>
2020-07-04 17:16:36 +0200

    translations work for one sentence

--- src/layouts/Default.js
+++ src/layouts/Default.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import Header from '../components/Header'
-import NavBar from '../components/NavBar'
-import Footer from '../components/Footer'
-import '../styles/global.css'
+import React from "react"
+import "../i18n"
+import Header from "../components/Header"
+import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
+import "../styles/global.css"
 
 export default class extends React.Component {
-
-    render () {
-      return (
-        <>
-        <Header/>
-        <NavBar/>
+  render() {
+    return (
+      <>
+        <Header />
+        <NavBar />
         {this.props.children}
-        <Footer/>
-        </>
-      )
-    }
-
+        <Footer />
+      </>
+    )
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,47 @@
 import React from "react"
+import { useTranslation, withTranslation, Trans } from "react-i18next"
 
 import "../styles/global.css"
 import DefaultLayout from "../layouts/Default"
 
 import home_hero_image from "../images/home-hero.jpg"
 
-export class Home extends React.Component {
-  render() {
-    return (
-      <DefaultLayout>
-        <main className="mt-3">
-          {/* Hero Image */}
-          <figure className="antialiased">
-            <img
-              className="w-2/3 md:w-1/2 mx-auto my-5 object-cover object-top shadow-xl rounded-lg"
-              src={home_hero_image}
-              alt="Marta & Álvaro"
-            />
-          </figure>
+const Home = () => {
+  const { t } = useTranslation()
+  return (
+    <DefaultLayout>
+      <main className="mt-3">
+        {/* Hero Image */}
+        <figure className="antialiased">
+          <img
+            className="w-2/3 md:w-1/2 mx-auto my-5 object-cover object-top shadow-xl rounded-lg"
+            src={home_hero_image}
+            alt="Marta & Álvaro"
+          />
+        </figure>
 
-          {/* Hero Text */}
-          <section className="mt-3">
-            <p className="p-1 text-sm sm:text-base md:text-md lg:text-lg text-center font-semibold">
-              Celebra con nosotros el inicio de este viaje el día
-            </p>
-          </section>
+        {/* Hero Text */}
+        <section className="mt-3">
+          <p className="p-1 text-sm sm:text-base md:text-md lg:text-lg text-center font-semibold">
+            {t("Celebra con nosotros el inicio de este viaje el día")}
+          </p>
+        </section>
 
-          {/* Wedding Date */}
-          <section className="pt-3 pb-4 text-header">
-            <h1>
-              3 de Octubre
-              <br className="md:hidden" /> 2020
-            </h1>
-          </section>
-          <section>
-            <h2 className="text-center text-sm sm:text-base md:text-md lg:text-lg text-center font-semibold">
-              Porque toda aventura empieza con un SÍ.
-            </h2>
-          </section>
-        </main>
-      </DefaultLayout>
-    )
-  }
+        {/* Wedding Date */}
+        <section className="pt-3 pb-4 text-header">
+          <h1>
+            3 de Octubre
+            <br className="md:hidden" /> 2020
+          </h1>
+        </section>
+        <section>
+          <h2 className="text-center text-sm sm:text-base md:text-md lg:text-lg font-semibold">
+            Porque toda aventura empieza con un SÍ.
+          </h2>
+        </section>
+      </main>
+    </DefaultLayout>
+  )
 }
 
 export default Home
