5e454530cd17147b61d63c420b4ba4f9b58c7ea3
ohduran <alvaro.duranb@hotmail.com>
2020-07-06 22:45:57 +0200

    fix gatsby as per https://stackoverflow.com/questions/62375916/netlify-deploy-fails

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,4 @@
 import i18n from "i18next"
-// import Backend from "i18next-http-backend"
 import LanguageDetector from "i18next-browser-languagedetector"
 import { initReactI18next } from "react-i18next"
 
@@ -20,9 +19,6 @@ const resources = {
 }
 
 i18n
-  // load translation using http -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-http-backend
-  // .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
